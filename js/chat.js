8ebcd175e477f595add8e579885a7a9b8e02d79d
miguel009 <miguelfr0305@hotmail.com>
2020-10-19 17:26:50 -0600

    Agregado de chat

---
+++ js/chat.js
@@ -0,0 +1,46 @@
+const database = firebase.database();
+const chatRef = database.ref('chat');
+function init(){
+    var textbox = document.getElementById("textbox");
+    var enter = document.getElementById("enter"); 
+    chatroom();
+    enter.onclick = function(e){
+        if (textbox.value.trim() != "") {
+                var autoID = chatRef.push().key
+                chatRef.child(autoID).set({
+                        from: 'user',
+                        message: textbox.value,
+                        type: 1
+                    });
+        }
+        else
+        {
+            Swal.fire({
+                title: 'Error!',
+                text: 'Hay campos vacios',
+                icon: 'error',
+                confirmButtonText: 'Entendido'
+                })
+        }
+
+    };
+}
+
+function chatroom(){
+    chatRef.orderByKey().limitToLast(10).on('child_added', snapshot => {
+        if (snapshot.val().type == 1) {
+            document.getElementById("chat_room").innerHTML += "<span class='mymessage rounded text-white'>"+snapshot.val().message+"</span><br><br>";
+        } else {
+            document.getElementById("chat_room").innerHTML += "<span class='othermessage bg-secondary rounded text-white'>"+snapshot.val().message+"</span><br><br>";
+        }
+        console.log(snapshot.val().message);
+    });
+}
+
+
+if(window.addEventListener){
+    window.addEventListener("load", init, false);
+}
+   else if(window.attachEvent){
+    window.attachEvent("onload", init);
+}
--- js/home.js
+++ js/home.js
@@ -8,43 +8,47 @@ function colorCategory() {
         email:'',
         key: ''
     };
-    var reds= document.getElementById("red");
-    reds.innerHTML=" <div class='modal fade' id='exampleModal' tabindex='-1' role='dialog' aria-labelledby='exampleModalLabel' aria-hidden='true'>"+
-    "<div class='modal-dialog' role='document'>"+
-      "<div class='modal-content'>"
-        +"<div class='modal-header'>"
-          +"<h5 class='modal-title' id='exampleModalLabel'>Actualizar Contraseña</h5>"
-          +"<button type='button' class='close' data-dismiss='modal' aria-label='Close'>"
-            +"<span aria-hidden='true'>&times;</span>"
-         +"</button>"
-        +"</div>"
-        +"<div class='modal-body'>"
-        +"<form>"
-            +"<div class='form-group'>"
-              +"<label for='recipient-name' class='col-form-label'>Email:</label>"
-              +"<input type='email' class='form-control' id='emails' disabled>"
-           +"</div>"
-           +"<div class='form-group'>"
-           +"<label for='recipient-name' class='col-form-label'>Contraseña antigua:</label>"
-           +"<input type='password' class='form-control' id='contraseña'>"
+    try {
+        var reds= document.getElementById("red");
+        reds.innerHTML=" <div class='modal fade' id='exampleModal' tabindex='-1' role='dialog' aria-labelledby='exampleModalLabel' aria-hidden='true'>"+
+        "<div class='modal-dialog' role='document'>"+
+        "<div class='modal-content'>"
+            +"<div class='modal-header'>"
+            +"<h5 class='modal-title' id='exampleModalLabel'>Actualizar Contraseña</h5>"
+            +"<button type='button' class='close' data-dismiss='modal' aria-label='Close'>"
+                +"<span aria-hidden='true'>&times;</span>"
+            +"</button>"
+            +"</div>"
+            +"<div class='modal-body'>"
+            +"<form>"
+                +"<div class='form-group'>"
+                +"<label for='recipient-name' class='col-form-label'>Email:</label>"
+                +"<input type='email' class='form-control' id='emails' disabled>"
             +"</div>"
             +"<div class='form-group'>"
-            +"<label for='recipient-name' class='col-form-label'>Contraseña nueva:</label>"
-            +"<input type='password' class='form-control' id='contraseñaN'>"
-             +"</div>"
-             +"<div class='form-group'>"
-             +"<label for='recipient-name' class='col-form-label'>Contraseña nueva repeticion:</label>"
-             +"<input type='password' class='form-control' id='contraseñaN2'>"
-              +"</div>"
-          +"</form>"
+            +"<label for='recipient-name' class='col-form-label'>Contraseña antigua:</label>"
+            +"<input type='password' class='form-control' id='contraseña'>"
+                +"</div>"
+                +"<div class='form-group'>"
+                +"<label for='recipient-name' class='col-form-label'>Contraseña nueva:</label>"
+                +"<input type='password' class='form-control' id='contraseñaN'>"
+                +"</div>"
+                +"<div class='form-group'>"
+                +"<label for='recipient-name' class='col-form-label'>Contraseña nueva repeticion:</label>"
+                +"<input type='password' class='form-control' id='contraseñaN2'>"
+                +"</div>"
+            +"</form>"
+            +"</div>"
+            +"<div class='modal-footer'>"
+                +"<button type='button' id='enviars' class='btn btn-success'>Enviar</button>"
+            +"<button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button>"
+            +"</div>"
         +"</div>"
-        +"<div class='modal-footer'>"
-            +"<button type='button' id='enviars' class='btn btn-success'>Enviar</button>"
-          +"<button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button>"
         +"</div>"
-      +"</div>"
-    +"</div>"
-  +"</div>";
+    +"</div>";
+    } catch (error) {
+        
+    }
     user= JSON.parse(localStorage.getItem("user"));
     var title = document.getElementsByClassName("nav-link");
     var signout = document.getElementById("buton-menu");
@@ -56,7 +60,7 @@ function colorCategory() {
         title[2].textContent = "Iniciar Sesion";
         signout.textContent = "Registrarme";
     }
-
+    
     signout.onclick = function(e){
         if (signout.textContent == "Registrarme") {
             window.location.href = "./register.html";
